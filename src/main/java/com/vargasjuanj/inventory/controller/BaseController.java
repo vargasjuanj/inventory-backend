e2f76a947584e2bc72ce696590290adbc1242ca1
vargasjuanj <juanjovargas443377@gmail.com>
2024-03-12 22:42:00 -0300

    CE2-0000 Se corrige update para que no cree otro objeto usando reflexion y se añade a la respuesta del delete el ResponseEntity

--- src/main/java/com/vargasjuanj/inventory/controller/BaseController.java
+++ src/main/java/com/vargasjuanj/inventory/controller/BaseController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin  (origins = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin  (origins = {"http://localhost:4200"}, methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class BaseController<E, S extends IBaseService<E>> {
 	@Autowired	
 	protected S service;
--- src/main/java/com/vargasjuanj/inventory/controller/CategoryController.java
+++ src/main/java/com/vargasjuanj/inventory/controller/CategoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
-@CrossOrigin(origins = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin  (origins = {"http://localhost:4200"}, methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class CategoryController extends BaseController<Category, CategoryServiceImpl> {
 
 }
